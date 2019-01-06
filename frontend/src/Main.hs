f411a47a46cdfb153922dce74e5114303ac1544f
Christian Henry <christian.henry92@gmail.com>
2019-01-05 20:41:11 -0500

    Previous change never loaded, try this out

--- frontend/src/Main.hs
+++ frontend/src/Main.hs
@@ -6,6 +6,6 @@ import Reflex.Dom.Core
 import Language.Javascript.JSaddle.Warp
 
 main :: IO ()
-main = run 3003 $ mainWidget $ do 
+main = mainWidget $ do 
   text "Deployed to github-pages!!!"
   return ()

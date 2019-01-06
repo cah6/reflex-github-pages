51c558cbe2b044dd46e2abafe06fabefd3f0c308
Christian Henry <christian.henry92@gmail.com>
2019-01-05 20:44:11 -0500

    Try without changing the warp run thing

--- frontend/src/Main.hs
+++ frontend/src/Main.hs
@@ -6,6 +6,6 @@ import Reflex.Dom.Core
 import Language.Javascript.JSaddle.Warp
 
 main :: IO ()
-main = mainWidget $ do 
-  text "Deployed to github-pages!!!"
+main = run 3003 $ mainWidget $ do 
+  text "Deployed to github-pages with warp run!"
   return ()
